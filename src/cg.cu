fb3a5712a46a2b0a3d62afca285e15d14e8a1e94
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-01-13 18:09:43 -0600

    Add initial d calculation

--- src/cg.cu
+++ src/cg.cu
@@ -11,23 +11,36 @@ template <typename T> struct Device_buffers {
 
     Device_buffers(std::int64_t n) {
         cudaMalloc(&r_d, sizeof(T) * n);
+        cudaMalloc(&s_d, sizeof(T) * n);
+        cudaMalloc(&d_d, sizeof(T) * n);
 
         cusparseCreateDnVec(&r, n, r_d, cuda_type);
+        cusparseCreateDnVec(&s, n, s_d, cuda_type);
+        cusparseCreateDnVec(&d, n, d_d, cuda_type);
     }
 
     ~Device_buffers() {
         cudaFree(r_d);
+        cudaFree(s_d);
+        cudaFree(d_d);
 
         cusparseDestroyDnVec(r);
+        cusparseDestroyDnVec(s);
+        cusparseDestroyDnVec(d);
     }
 
     cusparseDnVecDescr_t r;
+    cusparseDnVecDescr_t s;
+    cusparseDnVecDescr_t d;
+
     T *r_d;
+    T *s_d;
+    T *d_d;
 };
 } // namespace
 
 namespace cg_run {
-// R is lower triangular incomplete Cholesky where A ~= M = R^TR
+// L is lower triangular incomplete Cholesky where A ~= M = L * L'
 int cg(cusparseHandle_t cusparse, cublasHandle_t cublas, cusparseSpMatDescr_t A,
        cusparseDnVecDescr_t b, cusparseDnVecDescr_t x, cusparseSpMatDescr_t L,
        double tolerance, int max_iterations, bool real_residual) {
@@ -68,12 +81,60 @@ int cg(cusparseHandle_t cusparse, cublasHandle_t cublas, cusparseSpMatDescr_t A,
     double residual_norm = 0;
     CUBLAS_CHECK(cublasDnrm2_v2(cublas, n, d.r_d, 1, &residual_norm));
 
+    // d = L' (L \ r)
+    // Solve M = L * L' which approximates A solve
+    // Since SpSV supports in-place operations, we perform the solve like so:
+    //   d = L \ r
+    //   d = L' \ d
+    void *buffer_SpSV = nullptr;
+
+    std::size_t bufsize_SpSV_L = 0;
+    std::size_t bufsize_SpSV_LT = 0;
+
+    cusparseSpSVDescr_t desc_SpSV_L;
+    cusparseSpSVDescr_t desc_SpSV_LT;
+    CUSPARSE_CHECK(cusparseSpSV_createDescr(&desc_SpSV_L));
+    CUSPARSE_CHECK(cusparseSpSV_createDescr(&desc_SpSV_LT));
+
+    constexpr double alpha_SpSM = 1.0;
+
+    // Reuse larger of two buffers
+    CUSPARSE_CHECK(cusparseSpSV_bufferSize(
+        cusparse, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha_SpSM, L, d.r, d.d,
+        cuda_type, CUSPARSE_SPSV_ALG_DEFAULT, desc_SpSV_L, &bufsize_SpSV_L));
+    CUSPARSE_CHECK(cusparseSpSV_bufferSize(
+        cusparse, CUSPARSE_OPERATION_TRANSPOSE, &alpha_SpSM, L, d.d, d.d,
+        cuda_type, CUSPARSE_SPSV_ALG_DEFAULT, desc_SpSV_LT, &bufsize_SpSV_LT));
+    const std::size_t bufsize_SpSV = std::max(bufsize_SpSV_L, bufsize_SpSV_LT);
+    CUDA_CHECK(cudaMalloc(&buffer_SpSV, bufsize_SpSV));
+
+    // Analysis
+    CUSPARSE_CHECK(cusparseSpSV_analysis(
+        cusparse, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha_SpSM, L, d.r, d.d,
+        cuda_type, CUSPARSE_SPSV_ALG_DEFAULT, desc_SpSV_L, buffer_SpSV));
+    CUSPARSE_CHECK(cusparseSpSV_analysis(
+        cusparse, CUSPARSE_OPERATION_TRANSPOSE, &alpha_SpSM, L, d.d, d.d,
+        cuda_type, CUSPARSE_SPSV_ALG_DEFAULT, desc_SpSV_LT, buffer_SpSV));
+
+    // Solve
+    CUSPARSE_CHECK(cusparseSpSV_solve(
+        cusparse, CUSPARSE_OPERATION_NON_TRANSPOSE, &alpha_SpSM, L, d.r, d.d,
+        cuda_type, CUSPARSE_SPSV_ALG_DEFAULT, desc_SpSV_L));
+    CUSPARSE_CHECK(cusparseSpSV_solve(cusparse, CUSPARSE_OPERATION_TRANSPOSE,
+                                      &alpha_SpSM, L, d.d, d.d, cuda_type,
+                                      CUSPARSE_SPSV_ALG_DEFAULT, desc_SpSV_LT));
+
+    // TODO: delta_new
+
     int iterations = 0;
     while (iterations < max_iterations) {
         iterations += 1;
         // TODO: Implement solver loop
     }
 
+    CUSPARSE_CHECK(cusparseSpSV_destroyDescr(desc_SpSV_LT));
+    CUSPARSE_CHECK(cusparseSpSV_destroyDescr(desc_SpSV_L));
+    CUDA_CHECK(cudaFree(buffer_SpSV));
     CUDA_CHECK(cudaFree(buffer_residual_MV));
 
     return iterations;
