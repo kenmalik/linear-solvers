b7a300a1f5d224550cb5b86b65b2d1bdccd5b0f5
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-02-17 12:22:58 -0800

    Add inputs to nsys-profile.sh

--- profile/nsys-profile.sh
+++ profile/nsys-profile.sh
@@ -2,10 +2,25 @@
 
 set -e
 
-report_name="cg_nvtx"
+data_path=$(realpath ${1:-"test/data/1138_bus.mat"})
+data_base=$(basename "$data_path" .mat)
+data_dir=$(dirname "$data_path")
 
-nsys profile -f true -o $report_name -t cuda,nvtx build/cgrun --real-residual -b test/data/1138_bus_b.mat test/data/1138_bus.mat test/data/1138_bus_ichol.mat
+prec_type=$2
+prec_path=${2:+"${data_dir}/${data_base}_${2}.mat"}
 
-nsys stats -o "cg" --force-overwrite true --force-export true -f csv -r nvtx_sum "${report_name}.nsys-rep"
-nsys stats -o "cg" --force-overwrite true --force-export true -f csv -r cuda_api_sum --filter-nvtx "pre-cg" "${report_name}.nsys-rep"
-nsys stats -o "cg" --force-overwrite true --force-export true -f csv -r cuda_api_sum --filter-nvtx "post-cg" "${report_name}.nsys-rep"
+gpu=$(nvidia-smi --query-gpu name --format csv,noheader | sed 's/NVIDIA //g' | tr ' ' '_')
+
+formatted="runtimes-cg-cuda-${data_base}-${prec_type:-"identity"}-${gpu}-"
+
+nsys profile -f true -o $formatted -t cuda,nvtx \
+    build/cgrun --real-residual -b "${data_dir}/${data_base}_b.mat" "${data_dir}/${data_base}.mat" $prec_path
+
+nsys stats -o $formatted --force-overwrite true --force-export true -f csv \
+    -r nvtx_sum "${formatted}.nsys-rep"
+nsys stats -o $formatted --force-overwrite true --force-export true -f csv \
+    -r cuda_api_sum --filter-nvtx "pre-cg" "${formatted}.nsys-rep"
+nsys stats -o $formatted --force-overwrite true --force-export true -f csv \
+    -r cuda_api_sum --filter-nvtx "post-cg" "${formatted}.nsys-rep"
+
+echo nsys profile -f true -o $formatted -t cuda,nvtx build/cgrun --real-residual -b "${data_dir}/${data_base}_b.mat" "${data_dir}/${data_base}.mat" $prec_path
