325e090f0e2839408cb84999d5ba4e1d50ba3b70
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-03-13 13:21:06 -0700

    Remove single-precision DR-BCGs

--- algorithms/dr-bcg/cuda/include/dr_bcg/cuda.h
+++ algorithms/dr-bcg/cuda/include/dr_bcg/cuda.h
@@ -4,27 +4,14 @@
 #include <cusolverDn.h>
 #include <cusparse_v2.h>
 
-#include <functional>
-
 namespace dr_bcg::cuda {
 
-int dr_bcg(cusparseSpMatDescr_t A, cusparseDnMatDescr_t X,
-           cusparseDnMatDescr_t B, float tolerance = 1e-6,
-           int max_iterations = 100,
-           std::function<void(int, float)> residual_callback = nullptr);
-
-int dr_bcg(cusparseSpMatDescr_t A, cusparseDnMatDescr_t X,
-           cusparseDnMatDescr_t B, double tolerance = 1e-6,
-           int max_iterations = 100,
-           std::function<void(int, double)> residual_callback = nullptr);
+int solve(cusparseSpMatDescr_t A, cusparseDnMatDescr_t X,
+          cusparseDnMatDescr_t B, double tolerance = 1e-6,
+          int max_iterations = 100);
 
 int solve(cusparseSpMatDescr_t A, cusparseDnMatDescr_t X,
-           cusparseDnMatDescr_t B, cusparseSpMatDescr_t L,
-           double tolerance = 1e-6, int max_iterations = 100);
-
-int dr_bcg(cusparseSpMatDescr_t A, cusparseDnMatDescr_t X,
-           cusparseDnMatDescr_t B, cusparseSpMatDescr_t L,
-           float tolerance = 1e-6, int max_iterations = 100,
-           std::function<void(int, float)> residual_callback = nullptr);
+          cusparseDnMatDescr_t B, cusparseSpMatDescr_t L,
+          double tolerance = 1e-6, int max_iterations = 100);
 
 } // namespace dr_bcg::cuda
