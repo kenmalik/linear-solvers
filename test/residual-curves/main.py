cc51b64f912f72b54df896bf9bc0303755640d21
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-03-14 15:36:53 -0700

    Add option to read files instead of running cgrun

--- test/residual-curves/main.py
+++ test/residual-curves/main.py
@@ -1,6 +1,7 @@
 import itertools
 import argparse
 import subprocess
+from pathlib import Path
 
 import pandas as pd
 
@@ -13,13 +14,46 @@ impls = ["mkl", "cuda"]
 
 class Args(argparse.Namespace):
     dataset: str
+    files: list[Path]
+
+
+def valid_path(arg: str):
+    p = Path(arg)
+    if not p.exists() or not p.is_file():
+        raise argparse.ArgumentTypeError(f"{arg} is an invalid path")
+    return p
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dataset", default="1138_bus")
-    args = parser.parse_args(namespace=Args)
+    parser.add_argument("-f", "--files", nargs="*", type=valid_path)
+    args = parser.parse_args(namespace=Args())
 
+    if args.files:
+        from_files(args)
+    else:
+        from_cgrun(args)
+
+
+def from_files(args: Args):
+    data = {}
+
+    dataset = ""
+    for file in args.files:
+        alg, impl, new_dataset = file.stem.split("_", maxsplit=2)
+        if len(dataset) != 0 and new_dataset != dataset:
+            raise ValueError(f"mismatching dataset '{new_dataset}'")
+        else:
+            dataset = new_dataset
+
+        residuals = pd.read_csv(file).astype(float)
+        data.setdefault(alg, []).append((impl, residuals))
+
+    plot(data, dataset)
+
+
+def from_cgrun(args: Args):
     data = {}
     for alg, impl in itertools.product(algs, impls):
         prog = [
