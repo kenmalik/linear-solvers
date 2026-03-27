63caef8fd0effcc4432419b54ab96faf7875791b
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-03-27 14:55:25 -0700

    Update residual_curves.py to label with meta info

--- scripts/residual_curves.py
+++ scripts/residual_curves.py
@@ -39,10 +39,10 @@ def read_files(files: list[Path]):
     data = {}
 
     for file in files:
-        _, alg, impl = file.stem.split("_")
+        _, alg, impl, *meta = file.stem.split("_")
 
         residuals = pd.read_csv(file).astype(float)
-        data.setdefault(alg, []).append((impl, residuals))
+        data.setdefault(alg, []).append((impl, residuals, meta))
 
     return data
 
@@ -50,22 +50,46 @@ def read_files(files: list[Path]):
 def plot(data: dict[str, list], args: Args):
     fig, ax = plt.subplots(len(data), 2, figsize=(8.5, 8.5))
 
-    for i, [alg, info] in enumerate(data.items()):
-        for impl, residuals in info:
-            ax[i, 0].plot(residuals, label=impl)
-            ax[i, 1].plot(np.log(residuals.to_numpy()), label=impl)
+    if len(data) > 1:
+        for i, [alg, info] in enumerate(data.items()):
+            for impl, residuals, meta in info:
+                label = impl
+                if meta:
+                    label += f" ({', '.join(meta)})"
+                ax[i, 0].plot(residuals, label=label)
+                ax[i, 1].plot(np.log(residuals.to_numpy()), label=label)
 
-        ax[i, 0].set_title(alg)
-        ax[i, 0].legend()
-        ax[i, 0].set_ylabel(r"$\frac{\Vert r \Vert}{\Vert b \Vert}$")
-        ax[i, 0].set_xlabel("Iteration")
+            ax[i, 0].set_title(alg)
+            ax[i, 0].legend()
+            ax[i, 0].set_ylabel(r"$\frac{\Vert r \Vert}{\Vert b \Vert}$")
+            ax[i, 0].set_xlabel("Iteration")
 
-        ax[i, 1].set_title(alg)
-        ax[i, 1].legend()
-        ax[i, 1].set_ylabel(
-            r"$\log \left( \frac{\Vert r \Vert}{\Vert b \Vert} \right)$"
-        )
-        ax[i, 1].set_xlabel("Iteration")
+            ax[i, 1].set_title(alg)
+            ax[i, 1].legend()
+            ax[i, 1].set_ylabel(
+                r"$\log \left( \frac{\Vert r \Vert}{\Vert b \Vert} \right)$"
+            )
+            ax[i, 1].set_xlabel("Iteration")
+    else:
+        for alg, info in data.items():
+            for impl, residuals, meta in info:
+                label = impl
+                if meta:
+                    label += f" ({', '.join(meta)})"
+                ax[0].plot(residuals, label=label)
+                ax[1].plot(np.log(residuals.to_numpy()), label=label)
+
+            ax[0].set_title(alg)
+            ax[0].legend()
+            ax[0].set_ylabel(r"$\frac{\Vert r \Vert}{\Vert b \Vert}$")
+            ax[0].set_xlabel("Iteration")
+
+            ax[1].set_title(alg)
+            ax[1].legend()
+            ax[1].set_ylabel(
+                r"$\log \left( \frac{\Vert r \Vert}{\Vert b \Vert} \right)$"
+            )
+            ax[1].set_xlabel("Iteration")
 
     title = "Residual Error Curve Comparison"
     if args.dataset:
