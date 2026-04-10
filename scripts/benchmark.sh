e0149e18944c33a4e8e5d5cebbc8382e14754ce6
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-04-10 13:30:56 -0700

    Get iters from benchmark data

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -7,5 +7,5 @@ FILENAME="benchmark_$(date +%F-%H-%M-%S)"
 
 "${BUILD_DIR}/benchmark/solver_benchmarks" --benchmark_out="${FILENAME}.json" --benchmark_out_format=json --benchmark_time_unit=ms
 
-jq "[.benchmarks | .[] | {name: .name, time: .real_time, time_unit: .time_unit}]" < "${FILENAME}.json" \
+jq "[.benchmarks | .[] | {name: .name, time: .real_time, time_unit: .time_unit, iters: .iters}]" < "${FILENAME}.json" \
     | python "${SCRIPT_DIR}/runtime_comparison.py" -o "${FILENAME}.png"
