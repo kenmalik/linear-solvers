938231711cd068fcd400b130be093cdf2073458b
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-04-10 08:52:44 -0700

    Pipe benchmark jq output into plotting script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -7,4 +7,5 @@ FILENAME="benchmark_$(date +%F-%H-%M-%S).json"
 
 "${BUILD_DIR}/benchmark/solver_benchmarks" --benchmark_out="$FILENAME" --benchmark_out_format=json --benchmark_time_unit=ms
 
-jq "[.benchmarks | .[] | {name: .name, time: .real_time, time_unit: .time_unit}]" < "$FILENAME"
+jq "[.benchmarks | .[] | {name: .name, time: .real_time, time_unit: .time_unit}]" < "$FILENAME" \
+    | python "${SCRIPT_DIR}/runtime_comparison.py"
