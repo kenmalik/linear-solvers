83a1b046d0ba7cf8354b6591fc05ddee28c56d8e
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2026-05-29 12:41:57 -0500

    Move copy_upper_triangular into qr.h

--- algorithms/dr-bcg/cuda/helper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cuda_runtime.h>
-
-void copy_upper_triangular(float *dst, const float *src, int ld_src, int n,
-                           cudaStream_t stream);
-
-void copy_upper_triangular(double *dst, const double *src, int ld_src, int n,
-                           cudaStream_t stream);
--- algorithms/dr-bcg/cuda/qr.h
+++ algorithms/dr-bcg/cuda/qr.h
@@ -3,7 +3,6 @@
 #include "common/cuda_checks.h"
 #include "common/cuda_event_timer.h"
 #include "common/type_info.h"
-#include "helper.h"
 
 template <SupportedType T>
 struct HouseholderQrWorkspace {
@@ -132,6 +131,27 @@ struct CholQrWorkspace {
             free(h_work);
     }
 };
+
+template <SupportedType T>
+__global__ void copy_upper_triangular_kernel(T *dst, const T *src,
+                                                    const int ld_src, const int n) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < n && col < n) {
+        dst[row + col * n] = (row <= col) ? src[row + col * ld_src] : 0.0;
+    }
+}
+
+template <SupportedType T>
+void copy_upper_triangular(T *dst, const T *src, int ld_src, int n,
+                           cudaStream_t stream) {
+    constexpr int block_n = 16;
+    constexpr dim3 block_dim(block_n, block_n);
+    dim3 grid_dim((n + block_n - 1) / block_n, (n + block_n - 1) / block_n);
+    copy_upper_triangular_kernel<<<grid_dim, block_dim, 0, stream>>>(
+        dst, src, ld_src, n);
+}
 
 template <SupportedType T>
 void cholesky_qr(T *&d_Q, const T *&d_A, const int &m, const int &n, cudaStream_t &stream,
